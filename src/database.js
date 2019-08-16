08e32340df60155e9ff20b6e2f629a6e4d9f5220
fivetars <fivetarstechnology@gmail.com>
2019-08-16 15:15:40 -0500

    prueba

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://mon:27017/notes-db-app',{
+//mongoose.connect('mongodb://localhost/bd', {
+mongoose.connect('mongodb://mon:27017/notes-db-app', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
 })
 
-    .then(db => console.log('DB is conect'))
-    .catch(err=> console.error(err));
+    .then(bd => console.log('Base de datos conectada'))
+    .catch(err => console.error(err));
